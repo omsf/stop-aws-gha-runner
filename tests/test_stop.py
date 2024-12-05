f361af09e5fee8412dbcc8a5f810d40716976ac6
Ethan Holz <ethan.holz@omsf.io>
2024-12-05 16:26:32 -0700

    test: use a fixture instead

--- tests/test_stop.py
+++ tests/test_stop.py
@@ -1,50 +1,45 @@
 from moto import mock_aws
 import boto3
 from stop_aws_gha_runner.stop import StopAWS
+import pytest
 
 
-@mock_aws
-def test_remove_instance():
-    ec2 = boto3.client("ec2", "us-west-2")
-    response = ec2.run_instances(
-        ImageId="ami-12345678",
-        MinCount=1,
-        MaxCount=1,
-    )
-    ids = [response["Instances"][0]["InstanceId"]]
-    StopAWS(region_name="us-west-2", instance_mapping={}).remove_instances(ids)
+@pytest.fixture(scope="function")
+def aws():
+    with mock_aws():
+        region_name = "us-west-2"
+        ec2 = boto3.client("ec2", region_name)
+        response = ec2.run_instances(
+            ImageId="ami-12345678",
+            MinCount=1,
+            MaxCount=1,
+        )
+        ids = [response["Instances"][0]["InstanceId"]]
+        stop_aws = StopAWS(region_name=region_name, instance_mapping={})
+        yield stop_aws, ids, ec2
+
+
+def test_remove_instance(aws):
+    stop_aws, ids, ec2 = aws
+    stop_aws.remove_instances(ids)
     status = ec2.describe_instance_status(InstanceIds=ids)
     assert (
         status["InstanceStatuses"][0]["InstanceState"]["Name"] == "terminated"
     )
 
 
-@mock_aws
-def test_wait_until_removed():
-    region = "us-west-2"
-    ec2 = boto3.client("ec2", region)
-    response = ec2.run_instances(
-        ImageId="ami-12345678",
-        MinCount=1,
-        MaxCount=1,
-    )
-    ids = [response["Instances"][0]["InstanceId"]]
-    stop_aws = StopAWS(region_name=region, instance_mapping={})
+def test_wait_until_removed(aws):
+    stop_aws, ids, ec2 = aws
     stop_aws.remove_instances(ids)
     stop_aws.wait_until_removed(ids)
-
-
-@mock_aws
-def test_wait_until_removed_kwargs():
-    region = "us-west-2"
-    ec2 = boto3.client("ec2", region)
-    response = ec2.run_instances(
-        ImageId="ami-12345678",
-        MinCount=1,
-        MaxCount=1,
+    status = ec2.describe_instance_status(InstanceIds=ids)
+    assert (
+        status["InstanceStatuses"][0]["InstanceState"]["Name"] == "terminated"
     )
-    ids = [response["Instances"][0]["InstanceId"]]
-    stop_aws = StopAWS(region_name=region, instance_mapping={})
+
+
+def test_wait_until_removed_kwargs(aws):
+    stop_aws, ids, _ = aws
     stop_aws.remove_instances(ids)
     kwargs = {"MaxAttempts": 2}
     stop_aws.wait_until_removed(ids, **kwargs)
