731d56360295740242fbb91835a1882c1f425726
Ethan Holz <ethan.holz@omsf.io>
2025-03-17 16:27:25 -0600

    chore: update gha-runner syntax

--- src/stop_aws_gha_runner/__main__.py
+++ src/stop_aws_gha_runner/__main__.py
@@ -11,26 +11,32 @@ def main():
     # Check for required environment variables
     check_required(env, required)
     # Build the environment variables
-    cloud_params = (
+
+    builder = (
         EnvVarBuilder(env)
         # This is the default case
-        .with_var("AWS_REGION", "region_name")
+        .update_state("AWS_REGION", "region_name")
         # This is the input case
-        .with_var("INPUT_AWS_REGION_NAME", "region_name")
+        .update_state("INPUT_AWS_REGION_NAME", "region_name")
         # This is the base case
-        .with_var("GITHUB_REPOSITORY", "repo")
+        .update_state("GITHUB_REPOSITORY", "repo")
         # This is the input case
-        .with_var("INPUT_GH_REPO", "repo")
-        .with_var("INPUT_INSTANCE_MAPPING", "instance_mapping", is_json=True)
-        .build()
+        .update_state("INPUT_GH_REPO", "repo")
+        .update_state(
+            "INPUT_INSTANCE_MAPPING", "instance_mapping", is_json=True
+        )
     )
 
     token = os.environ["GH_PAT"]
     # This is not needed by the StopAWS class
+    cloud_params = builder.params
     repo = cloud_params.pop("repo")
 
     gh = GitHubInstance(token, repo)
     deployment = TeardownInstance(StopAWS, cloud_params, gh)
+    deployment = TeardownInstance(
+        provider_type=StopAWS, cloud_params=cloud_params, gh=gh
+    )
     deployment.stop_runner_instances()
 
 
